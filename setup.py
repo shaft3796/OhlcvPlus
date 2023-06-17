9632143b160e3de21a43b75c1afdc1981737ec14
Shaft <sh4ft.me@gmail.com>
2023-06-17 19:27:01 +0200

    V2.0.1 Add project description.

--- setup.py
+++ setup.py
@@ -1,8 +1,14 @@
 from setuptools import setup
+from pathlib import Path
 
-setup(name='OhlcvPlus',
-      version='2.0.0',
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+setup(name='ohlcv-plus',
+      version='2.0.1',
       description='Crypto OHLCV data downloader.',
+      long_description=long_description,
+      long_description_content_type='text/markdown',
       author='Shaft',
       url='https://github.com/Shaft-3796/OHLCV-Plus',
       download_url='https://github.com/Shaft-3796/OHLCV-Plus/archive/refs/tags/2.0.0.tar.gz',
